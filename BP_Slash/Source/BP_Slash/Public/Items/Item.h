58380f508c60506887887ccfe4dd67b9da3bd83c
shuaib <ssolker@gmail.com>
2023-02-20 14:28:16 -0500

    End of 56

--- BP_Slash/Source/BP_Slash/Public/Items/Item.h
+++ BP_Slash/Source/BP_Slash/Public/Items/Item.h
@@ -11,17 +11,24 @@ class BP_SLASH_API AItem : public AActor
 {
 	GENERATED_BODY() //Reflection System, Work with BP too
 	
-public:	
+public:
 	AItem();
 	virtual void Tick(float DeltaTime) override;
 protected:
 	virtual void BeginPlay() override;
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Sine Parameters")
+		float Amplitude = 0.25f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Sine Parameters")
+		float TimeConstant = 5.f;
+
+	UFUNCTION(BlueprintPure)
+		float TransformedSin();
+
+	UFUNCTION(BlueprintPure)
+		float TransformedCos();
 private:
-	UPROPERTY(VisibleAnywhere);
-	float RunningTime;
-	UPROPERTY(EditAnywhere); //Edit only in blueprint editor not in instance (Only in default) EditAnywhere in both
-	float Amplitude = 0.25f;
-	UPROPERTY(EditAnywhere);
-	float TimeConstant = 5.f;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+		float RunningTime;
 };
