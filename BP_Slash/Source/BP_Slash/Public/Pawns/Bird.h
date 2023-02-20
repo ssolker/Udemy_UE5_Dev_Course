f2d56097cc7628493236f5885dd90e82d344debc
shuaib <ssolker@gmail.com>
2023-02-20 15:41:59 -0500

    End of 62

---
+++ BP_Slash/Source/BP_Slash/Public/Pawns/Bird.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "Components/CapsuleComponent.h"
+
+#include "Bird.generated.h" //this needs to be last one
+
+UCLASS()
+class BP_SLASH_API ABird : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	ABird();
+	virtual void Tick(float DeltaTime) override;
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(VisibleAnywhere)
+	UCapsuleComponent* Capsule;
+};
